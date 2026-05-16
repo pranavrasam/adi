fb93aca49d5b137c12d10008543ef724a3efa220
pranavrasam <pranavrasam777@gmail.com>
2026-05-17 02:43:25 +0530

    Update scriptp.js

--- scriptp.js
+++ scriptp.js
@@ -2,7 +2,7 @@ function checkPassword(){
     let password=document.getElementById("password").value;
     let correctPassword="Adityagay";
     if(password === correctPassword){
-        window.location.href="index.html";
+        window.location.href="server.html";
     }
     else{
 
